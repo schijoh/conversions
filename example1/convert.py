adbf0e3c49aeecf2e34dbf75ad28b21123b69d7e
schijoh <johannaschiele@hks.harvard.edu>
2021-01-18 12:51:15 +0100

    did example 1

--- example1/convert.py
+++ example1/convert.py
@@ -5,9 +5,13 @@ import json
 
 # use csv.DictReader to load input.csv
 # create a variable 'rows' with all of the rows in the csv
+with open("input.csv", "r") as f:
+	reader = csv.DictReader(f)
+	rows = list(reader)
+
+#print(rows)
 
 # loop through each row of the input which is in the format
-
 # {
 #   'title': ..., 
 #   'subtitle': ..., 
@@ -28,4 +32,27 @@ import json
 #  'markers': [...],
 # }
 
+data = []
+for row in rows:
+	print(row)
+	datapoint = {
+		"title": row["title"],
+		"subtitle": row["subtitle"],
+		"ranges": [
+			float(row["range_min"]),
+			float(row["range_mid"]),
+			float(row["range_max"])],
+		"measures": [
+			float(row["measure_min"]),
+			float(row["measure_max"])],
+		"markers": row["markers"]
+		}
+	data.append(datapoint)
+
 # use json.dump to output, 'output.json'
+with open('output.json', 'w') as jsonfile:
+    json.dump(data, jsonfile, indent=4)
+
+
+
+
